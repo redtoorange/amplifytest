c3084748cb5a20aea01e771172eee55712a232ee
Andrew McGuiness <andrew.mcguiness@rtslabs.com>
2020-10-02 09:54:43 -0400

    Added test GET

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Axios from "axios";
 
-function App() {
-    return (
-        <div>
-            <header>
-                Header
-            </header>
+const BASE_URL = "https://civ-roulette.firebaseio.com/";
 
-            <main>
-                Main
-            </main>
 
-            <footer>
-                Footer
-            </footer>
-        </div>
-    );
+interface AppState {
+    data: string;
+}
+
+class App extends Component<any, AppState> {
+    state: AppState = {
+        data: ""
+    }
+
+    componentDidMount() {
+        Axios.get(BASE_URL + 'challenges.json')
+            .then(result => result.data)
+            .then(data => this.setState({data: JSON.stringify(data)}))
+    }
+
+    render() {
+        return (
+            <div>
+                <header>
+                    Header
+                </header>
+
+                <main>
+                    {this.state.data}
+                </main>
+
+                <footer>
+                    Footer
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
